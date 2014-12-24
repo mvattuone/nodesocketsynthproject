9ee8f0bc0b65f6ac1ca3a134a3209fe80c5c6f89
infinityfarm <aaronjblack@icloud.com>
2014-12-24 11:30:52 -0500

    codeschool node.js lessons 2.4

--- app.js
+++ app.js
@@ -1,10 +1,36 @@
+// create server
+
 var http = require('http');
 var fs = require('fs');
 
-http.createServer(function(request, response) {
-	response.writeHead(200);
+var server = http.createServer();
+
+server.listen(8080);
+server.on('request', function(request, response) {
+	response.writeHead(200, {
+		'Content-Type': 'text/html'
+	});
 	fs.readFile('index.html', function(err, contents) {
-		console.log(contents);
-	}); // asynchronously read index.html file
-	response.end();
-}).listen(8080);
+		response.write(contents);
+		response.end();
+	}); // asynchronously read index.html file	
+});
+
+// create custom chat EventEmitter
+
+var events = require('events');
+var EventEmitter = events.EventEmitter;
+var chat = new EventEmitter();
+var users = [], chatlog = [];
+
+chat.on('message', function(message) {
+	console.log(message);
+});
+
+chat.on('join', function(nickname) {
+	users.push(nickname);
+});
+
+chat.emit('join', 'custom message');
+
+chat.emit('message', 'custom message');
