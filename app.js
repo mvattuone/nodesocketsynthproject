abea34f76274f742f2c70afecf84684536288a93
infinityfarm <aaronjblack@icloud.com>
2014-12-25 18:50:17 -0500

    successfully got chatters list to update across two sockets from the same computer on localhost

--- app.js
+++ app.js
@@ -2,14 +2,55 @@ var express = require('express');
 var app = express();
 var server = require('http').createServer(app);
 var io = require('socket.io')(server);
+var redis = require('redis');
+var redisClient = redis.createClient();
+
+var messages = [];
+
+var storeMessage = function(name, data) {
+	var message = JSON.stringify({name: name, data: data});
+
+	redisClient.lpush('messages', message, function(err, response) {
+		redisClient.ltrim('messages', 0, 10);
+	});
+}
 
 io.sockets.on('connection', function(client) {
 	console.log('Client connected...');
-	client.on('messages', function(data) {
-		client.broadcast.emit('messages', data);
-	});
+
 	client.on('join', function(name) {
-		client.set('nickname', name);
+		client.nickname = name;
+		client.broadcast.emit('add chatter', name);
+		client.emit('add chatter', name);
+		
+		redisClient.lrange('messages', 0, -1, function(err, messages) {
+			messages = messages.reverse();
+			messages.forEach(function(message) {
+				message = JSON.parse(message);
+				client.emit('messages', message.name + ': ' + message.data);
+			});
+		});
+		redisClient.smembers('names', function(err, names) {
+			names.forEach(function(name) {
+				client.emit('add chatter', name);
+			});
+		});
+		redisClient.sadd('chatters', name);
+	});
+
+	client.on('messages', function(message) {
+		client.nickname(function(err, name) {
+			client.broadcast.emit('messages', name + ': ' + message);
+			storeMessage(name, message);
+		});
+	});
+
+	client.on('disconnect', function(name) {
+		client.get('nickname', function(err, name) {
+			client.broadcast.emit('remove chatter', name);
+
+			redisClient.srem('chatters', name);
+		});
 	});
 });
 
